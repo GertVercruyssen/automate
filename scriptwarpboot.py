561cd8c1af0f2d18ddbec5e434db4b0d40bab0db
gertcrossdream <128119847+gertcrossdream@users.noreply.github.com>
2023-11-30 15:10:29 +0900

    various fixes improvements and problem solving

--- scriptwarpboot.py
+++ scriptwarpboot.py
@@ -1,22 +1,28 @@
 import numpy as np
 from PIL import ImageGrab
 import pyautogui
-import cv2
 import time
 from time import sleep
 from AppKit import NSPasteboard, NSStringPboardType
 import datetime
 import sys
+import logging
     
 #real resolution = 2560 × 1600 fullscreen is 2880x1800, window size is 1878x1170
 #pyautogui.position() is from 0x0 to 1439x899
 #screen image is 2880x1800 4 channels
 def main(boot):
+    logging.basicConfig(
+        format='%(asctime)s %(levelname)-8s %(message)s',
+        datefmt='%m-%d %H:%M',
+        filename='output.log',
+        encoding='utf-8',
+        level=logging.INFO)
+
     if boot == "b":
         booteve()
         
-    formatted_time = datetime.datetime.now().strftime('%H:%M')
-    print(formatted_time + " starting mission running")
+    output('starting mission running')
     
     global docked
     docked = False
@@ -32,34 +38,34 @@ def main(boot):
     waitdockticker = 0
     global screenimage
     global missiontitle
+    global loopedsetdestination = 0 #counter to check if stuck on loop
+    global candecline = True #only once per run of the program
     last_time = time.time()
-    timetoquit = False
+    global timetoquit = False
     finished = False
-    #cv2.namedWindow ('window', cv2.WINDOW_NORMAL)
     starttime = last_time
     pyautogui.moveTo(953,516)
     pyautogui.click()
     
     while(not finished):
         screenimage = ImageGrab.grab()
-        screen = np.array(screenimage)
         deltat=time.time()-last_time
-        #print('Loop took {} seconds'.format(deltat))
+        #output('Loop took {} seconds'.format(deltat))
         last_time = time.time()
         if (last_time - starttime) > 28800: #quit after 8 hours
             timetoquit = True
                         
         toprightpixel = (213, 215, 216, 255)
         toprightpixelselected = (214, 214, 214, 255)
-        if toprightpixel == screenimage.getpixel((2833,660)) or toprightpixelselected == screenimage.getpixel((2833,660)):
-            #print('you are undocked')
+        if toprightpixel == screenimage.getpixel((2862,653)) or toprightpixelselected == screenimage.getpixel((2862,653)):
+            #output('you are undocked')
             undocked = True
         else:
             undocked = False
             
         if not undocked :
-            if toprightpixel == screenimage.getpixel((2843,672)) or toprightpixelselected == screenimage.getpixel((2843,672)):
-                #print('you are docked')
+            if toprightpixel == screenimage.getpixel((2862,672)) or toprightpixelselected == screenimage.getpixel((2862,672)):
+                #output('you are docked')
                 docked = True
             else:
                 docked = False
@@ -68,49 +74,49 @@ def main(boot):
         
         if undocked:
             cyanpixel = (117, 251, 253, 255)
-            if cyanpixel == screenimage.getpixel((1149,646)) and cyanpixel == screenimage.getpixel((1140,649)):
-                #print('you are cloaked by gate')
+            if cyanpixel == screenimage.getpixel((1149,646)) and cyanpixel == screenimage.getpixel((1140,649)): #if booster (1204,647) and (1201,647)
+                #output('you are cloaked by gate')
                 cloaked = True
             else:
                 cloaked = False
             
         if timetoquit == True:
-            print('time limit reached: quitting the game')
-            pyautogui.press('esc')
-            sleep(5)
-            pyautogui.moveTo(819, 872)
+            output('time limit reached: quitting the game')
+            pyautogui.moveTo(515, 304)
+            sleep(1)
             pyautogui.click()
             finished = True
         else:
-            #print('mouse is at position: {}'.format(pyautogui.position()))
+            #output('mouse is at position: {}'.format(pyautogui.position()))
             if waittimer <= 0:
                 waittimer = 0.0
                 nextaction()
             else:
-                #print('waittimer: {}'.format(waittimer))
-                #print('deltat: {}'.format(deltat))
+                #output('waittimer: {}'.format(waittimer))
+                #output('deltat: {}'.format(deltat))
                 waittimer = waittimer-deltat
                     
-    formatted_time = datetime.datetime.now().strftime('%H:%M')
-    print(formatted_time + " finished")
-        #cv2.imshow('window', cv2.cvtColor(screen, cv2.COLOR_BGR2RGB)) #WINDOW_NORMAL
-        #if cv2.waitKey(1) % 0xFF == ord('q'):
-        #    cv2.destroyAllWindows()
-        #    break
+    output('finished')
         
 def nextaction():
     global waittimer
     global missiontitle
     global waitdockticker
+    global screenimage
+    global actionlist
+    global loopedsetdestination
+    global candecline
+    global timetoquit
+    
     if len(actionlist) > 0:
         currentaction = actionlist[0]
         if not currentaction.startswith("wait"):
-            print('Performing action {}'.format(currentaction))
+            output('Performing action {}'.format(currentaction))
         actionlist.pop(0)
         if currentaction == "wait5":
             waittimer = 5.0
-        elif currentaction == "wait10":
-            waittimer = 10.0
+        elif currentaction == "wait75":
+            waittimer = 7.5
         elif currentaction == "wait25":
             waittimer = 2.5
         elif currentaction == "wait1":
@@ -120,16 +126,16 @@ def nextaction():
                 actionlist.insert(0,"waituntilundocked")
                 actionlist.insert(0,"wait5")
         elif currentaction == "startconvo":
-            pyautogui.moveTo(626, 735)
+            pyautogui.moveTo(626, 750)
             pyautogui.click(button='right')
             time.sleep(1)
-            pyautogui.moveTo(649, 771)
+            pyautogui.moveTo(649, 786)
+            pyautogui.click()
+            time.sleep(5)
+            pyautogui.moveTo(800, 885)
             pyautogui.click()
         elif currentaction == "acceptmission":
-            pyautogui.moveTo(817, 760)
-            pyautogui.click()
-        elif currentaction == "closeconvo":
-            pyautogui.moveTo(1152, 382)
+            pyautogui.moveTo(916, 884)
             pyautogui.click()
         elif currentaction == "pressbluebutton":
             pyautogui.moveTo(642, 598)
@@ -143,8 +149,8 @@ def nextaction():
             pyautogui.press('g')
             pyautogui.keyUp('optionleft')
             time.sleep(3)
-            pyautogui.moveTo(896, 661)
-            pyautogui.dragTo(908, 544, button='left')
+            pyautogui.moveTo(896, 665)
+            pyautogui.dragTo(894, 540, button='left')
             time.sleep(1)
             pyautogui.keyDown('optionleft')
             pyautogui.press('c')
@@ -154,13 +160,13 @@ def nextaction():
             pyautogui.press('g')
             pyautogui.keyUp('optionleft')
         elif currentaction == "closeconvo":
-            pyautogui.moveTo(1193, 379)
+            pyautogui.moveTo(1241, 338)
             pyautogui.click()
         elif currentaction == "setdestinationagent":
-            pyautogui.moveTo(626, 735)
+            pyautogui.moveTo(626, 750)
             pyautogui.click(button='right')
             time.sleep(1)
-            pyautogui.moveTo(649, 795)
+            pyautogui.moveTo(649, 810)
             pyautogui.click()
         elif currentaction == "undock":
             pyautogui.keyDown('optionleft')
@@ -181,40 +187,38 @@ def nextaction():
                 else:
                     actionlist.insert(1,"waituntildocked")
                     waitdockticker = waitdockticker+1
-                    #print('waitdockticker: {}'.format(waitdockticker))
+                    #output('waitdockticker: {}'.format(waitdockticker))
             if waitdockticker > 300: #if nothing happens for some time then autopilot
-                print('failsafe engaged: unstuck with autopilot !')
+                output('failsafe engaged: unstuck with autopilot !')
                 pyautogui.keyDown('ctrl')
                 pyautogui.press('s')
                 pyautogui.keyUp('ctrl')
                 pyautogui.moveTo(1131, 489) #select fomething on the overview just to be safe
                 pyautogui.click()
                 sleep(35)
-                pyautogui.keyDown('ctrl')
-                pyautogui.press('s')
-                pyautogui.keyUp('ctrl')
                 waitdockticker = 0
                     
         elif currentaction == "warpnext":
+            time.sleep(1)
             pyautogui.moveTo(1178, 406)
             pyautogui.click()
             time.sleep(5)
         elif currentaction == "readmissiontitle":
-            pyautogui.moveTo(960, 402)
+            pyautogui.moveTo(1014, 360)
             pyautogui.click(clicks=3, interval=0.25)
             pyautogui.keyDown('command')
             pyautogui.press('c')
             pyautogui.keyUp('command')
             missiontitle = getClipboard()
-            print('current mission is {}'.format(missiontitle))
-        elif currentaction == "prepmission":
+            output('current mission is {}'.format(missiontitle))
+        elif currentaction == "setmissionroute":
             #Currently only supporting drop-off missions
-            pyautogui.moveTo(1084, 475)
+            pyautogui.moveTo(1132, 439)
             pyautogui.click(button='right')
             time.sleep(1)
-            pyautogui.moveTo(1106, 511)
+            pyautogui.moveTo(1152, 474)
             pyautogui.click()
-            time.sleep(3)
+            time.sleep(1)
             actionlist.insert(0,"checkroute")
         elif currentaction == "startmission":
             actionlist.insert(0,"undock")
@@ -234,83 +238,109 @@ def nextaction():
             actionlist.insert(14,"waituntildocked")
         elif currentaction == "completemission":
             time.sleep(1)
-            pyautogui.moveTo(767, 761)
+            pyautogui.moveTo(866, 883)
             pyautogui.click()
             time.sleep(1)
         elif currentaction == "warpstart":
-            time.sleep(2)
+            time.sleep(1)
+            pyautogui.moveTo(565, 494)
+            time.sleep(1)
             pyautogui.moveTo(566, 495)
+            time.sleep(1)
             pyautogui.click(button='right')
             time.sleep(1)
-            if (144, 228, 106, 255) == screenimage.getpixel((1250,949)):
-                pyautogui.moveTo(586, 535)
+            screenimage = ImageGrab.grab()
+            if (196, 197, 197, 255) == screenimage.getpixel((1169,1081)):
+                pyautogui.moveTo(587, 535)
                 pyautogui.click()
             else:
-                pyautogui.moveTo(589, 507)
+                pyautogui.moveTo(587, 511)
                 pyautogui.click()
             time.sleep(5)
             pyautogui.moveTo(1131, 489) #select fomething un the overview just to be safe
             pyautogui.click()
         elif currentaction == "checkroute":
             route = checkforjumpword()
-            if route:
-                actionlist.insert(0,"collectcargo")
+            if not route:
+                actionlist.insert(0,"setmissionroute")
+                loopedsetdestination = loopedsetdestination+1
+                if loopedsetdestination > 4: #stuck in a loop, probably trying to go to lowsec
+                    loopedsetdestination = 0
+                    if candecline:
+                        #decline and clear convo, actionlist (soft reset)
+                        candecline = False
+                        pyautogui.moveTo(962, 885)
+                        time.sleep(1)
+                        takeScreenshot('declined')
+                        time.sleep(1)
+                        pyautogui.click()
+                        time.sleep(10)
+                        pyautogui.moveTo(980, 337)
+                        time.sleep(1)
+                        pyautogui.click()
+                        actionlist = ["wait1"]
+                    else:
+                        actionlist = ["wait1"]
+                        timetoquit = True
+                else:
+                    actionlist.insert(0,"setmissionroute")
             else:
-                actionlist.insert(0,"prepmission")
+                loopedsetdestination = 0
         else:
-            print('Error: undefined action on the action queue')
-            print(currentaction)
+            output('Error: undefined action on the action queue')
+            output(currentaction)
     else:
-        print('no actions left, starting new plan')
+        output('no actions left, starting new plan')
         plannextactions()
         
 def plannextactions():
     if docked == True:
         location = readMapLocation()
-        if location == "<url=showinfo:1531//60000289 alt='Current Station'>Jatate IV - Moon 17 - Prompt Delivery Storage</url>":
-            formatted_time = datetime.datetime.now().strftime('%H:%M')
-            print(formatted_time + " starting new mission plan")
+        if location == "<url=showinfo:1927//60008464 alt='Current Station'>Finid X - Moon 1 - Amarr Navy Assembly Plant</url>":
+            output('starting new mission plan')
             if (121, 157, 72, 255) == screenimage.getpixel((1439,1446)): #agent mission already accepted
-                print('finishing accepted mission')
+                output('finishing accepted mission')
                 actionlist.append("startconvo")
-                actionlist.append("wait10")
+                actionlist.append("wait75")
                 actionlist.append("readmissiontitle")
-                actionlist.append("wait25")
-                actionlist.append("prepmission")
+                actionlist.append("wait1")
+                actionlist.append("setmissionroute")
                 actionlist.append("wait25")
                 actionlist.append("closeconvo")
-                actionlist.append("wait25")
+                actionlist.append("wait1")
                 actionlist.append("startmission")
             else:
                 actionlist.append("startconvo") #agent mission offered or no agent mission
-                print('starting new mission')
-                actionlist.append("wait10")
+                output('starting new mission')
+                actionlist.append("wait75")
                 actionlist.append("readmissiontitle")
+                actionlist.append("wait1")
+                actionlist.append("setmissionroute")
                 actionlist.append("wait25")
                 actionlist.append("acceptmission")
                 actionlist.append("wait25")
-                actionlist.append("prepmission")
-                actionlist.append("wait25")
+                actionlist.append("collectcargo")
+                actionlist.append("wait1")
                 actionlist.append("closeconvo")
-                actionlist.append("wait25")
+                actionlist.append("wait1")
                 actionlist.append("startmission")
                 
         else:
-            print('at some random station? going home to agent')
+            output('at some random station? going home to agent')
             actionlist.append("setdestinationagent")
             actionlist.append("undock")
             actionlist.append("waituntilundocked")
             actionlist.append("warpstart")
             actionlist.append("waituntildocked")
     else:
-        print('at some random point in space? going home to agent')
+        output('at some random point in space? going home to agent')
         actionlist.append("setdestinationagent")
         actionlist.append("wait5")
         actionlist.append("warpstart")
         actionlist.append("waituntildocked")
         
 def readMapLocation():
-    print('checking location on the map')
+    output('checking location on the map')
     pyautogui.press('f10')
     time.sleep(5)
     pyautogui.moveTo(572, 409)
@@ -319,7 +349,7 @@ def readMapLocation():
     pyautogui.moveTo(607, 520)
     pyautogui.click()
     location = getClipboard()
-    print('maplocation is: {}'.format(location))
+    output('maplocation is: {}'.format(location))
     pyautogui.press('f10')
     time.sleep(3)
     return location
@@ -335,50 +365,56 @@ def generateHash(xfirst, xsecond, y):
 def getClipboard():
     pb = NSPasteboard.generalPasteboard()
     pbstring = pb.stringForType_(NSStringPboardType)
-    #print(type(text))
+    #output(type(text))
     #print u"Pastboard string: %s".encode("utf-8") % repr(pbstring)
     return pbstring
 
 def booteve():
-    formatted_time = datetime.datetime.now().strftime('%H:%M')
-    print(formatted_time + " i have awoken!")
-    #waituntilevening() //didn't work, use cron job that starts in the evening
+    output('I have awoken!')
+    takeScreenshot('test1')
     #open eve launcher
     pyautogui.keyDown('command')
+    time.sleep(1)
     pyautogui.press('space')
     pyautogui.keyUp('command')
+    time.sleep(1)
     pyautogui.write('eve-online')
     pyautogui.press('enter')
     #wait until ready
-    time.sleep(900) #15 minutes should be enough to boot and do small updates
+    time.sleep(600) #some minutes should be enough to boot and do small updates
+    takeScreenshot('test2')
     #launch game
+    output('launching game')
     pyautogui.moveTo(942, 400)
     time.sleep(1)
     pyautogui.click()
-    time.sleep(180)
+    time.sleep(120) #2 minutes to boot should be enough test
+    takeScreenshot('test3')
     #minimize launcher
-    pyautogui.moveTo(1318, 17)
-    time.sleep(2)
-    pyautogui.click()
-    time.sleep(2)
-    pyautogui.click()
-    time.sleep(5)
-    pyautogui.moveTo(996, 890)
-    time.sleep(2)
-    pyautogui.click()
-    time.sleep(2)
-    #select character ???
+    #output('minimize launcher')
+    #pyautogui.moveTo(1318, 17)
+    #time.sleep(2)
+    #pyautogui.click()
+    #time.sleep(2)
+    #pyautogui.click()
+    #time.sleep(5)
+    #pyautogui.moveTo(996, 890)
+    #time.sleep(2)
+    #pyautogui.click()
+    #time.sleep(2)
     #minimize chat
+    output('minimize chat')
     pyautogui.moveTo(810, 693)
     time.sleep(2)
     pyautogui.click()
     time.sleep(2)
+    takeScreenshot('test4')
     
 def waituntilevening():
     t= datetime.datetime.today()
     future = datetime.datetime(t.year,t.month,t.day,22,0) #set until desired hour
     seconds = (future-t).total_seconds()
-    print('waiting {} seconds to start the show'.format(seconds))
+    output('waiting {} seconds to start the show'.format(seconds))
     time.sleep(seconds)
     
 def checkforjumpword():
@@ -396,6 +432,16 @@ def checkforjumpword():
         return True
     else:
         return False
+        
+def output(text):
+    print(text)
+    logging.info(text)
+
+def takeScreenshot(name = 0):
+    if name == 0:
+        name = time.time()
+    screenshot = ImageGrab.grab()
+    screenshot.save(name+'.png')
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
